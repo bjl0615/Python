162d55a8791c3aa9d22326ec907fbfcd80da6d9f
bjl0615 <77454091+bjl0615@users.noreply.github.com>
2022-03-16 16:22:03 +0900

    operator

---
+++ day01/operator.py
@@ -0,0 +1,31 @@
+print(1+1) # 2
+print(3-2) # 1
+print(5*2) # 10
+print(6/3) #2
+
+print(2**3) #
+print(5%3) # 나머지 구하기 2
+print(10%3) # 1
+print(5//3) # 1
+print(10 // 3) # 3
+
+print(10 > 3) # true
+print(4 >= 7) # false
+print(10 < 3) # false
+print(5 <= 5) # true
+
+print (3 == 3) #true
+print (4 == 2) # false
+print (3 + 4 == 7) # True
+
+print(1 != 3) # true
+print(not(1 != 3)) # false
+
+print((3 > 0) and (3 < 5)) # true
+print((3>0) & (3 < 5)) # true
+
+print((3 > 0) or ( 3 < 5)) # true
+print((3 > 0) | (3 > 5)) # true
+
+print(5 > 4 > 3) # true
+print(5 > 4 > 7) # false
